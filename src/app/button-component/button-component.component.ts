92c6c38f45700402d8bdb2e0bf02973873602900
William <xebas610@gmail.com>
2023-06-07 12:15:11 -0500

    feat(frontend): add comments of the test

--- src/app/button-component/button-component.component.ts
+++ src/app/button-component/button-component.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-button-component',
+  selector: 'app-button-component', /* Selector para usarlo como etiqueta en appComponent */
   templateUrl: './button-component.component.html',
   styleUrls: ['./button-component.component.css']
 })
